006ff545742680240c105f5a8187610b61255350
tictactoeid <jungjimin424@naver.com>
2022-10-25 02:55:53 +0900

    implemented BST && debugging AVL about rotateLeft()

--- Assignment2/src/AVL.java
+++ Assignment2/src/AVL.java
@@ -1,40 +1,108 @@
 public class AVL extends BST
 {
-  public AVL() { }
+  public AVL() {
+    super();
+  }
 
   private int height(Node n){
-    if (n==null) return -1;
+    if (n == null) return 0;
     return n.height;
   }
 
-  private void updateHeights(Node n) {
-    if (height(n.leftChild) > height(n.rightChild)) {
-      n.height = 1+height(n.leftChild);
-    }
-    else {
-      n.height = 1+height(n.rightChild);
-    }
-
-    //1+max(l, r) => faster?
+  private void updateHeight(Node n) {
+    n.height = 1 + Math.max(height(n.leftChild), height(n.rightChild));
   }
 
   private Node rotateLeft(Node n) {
+    Node newParent = n.rightChild; // new parent
+    n.rightChild = newParent.leftChild;
+    newParent.leftChild = n;
 
+    updateHeight(n); // update new child first
+    updateHeight(newParent); // and new parent
+    return newParent;
   }
 
   private Node rotateRight(Node n) {
+    Node newParent = n.leftChild;
+    n.leftChild = newParent.rightChild;
+    newParent.rightChild = n;
 
+    updateHeight(n);
+    updateHeight(newParent);
+    return newParent;
   }
 
   private Node rotateLeftRight(Node n) {
-
+    n.leftChild = rotateLeft(n.leftChild);
+    return rotateRight(n);
   }
 
   private Node rotateRightLeft(Node n) {
-
+    n.rightChild = rotateRight(n.rightChild);
+    return rotateLeft(n);
   }
 
-  public void insert(String key) { }
+  public void insert(String key) {
+    if (root == null) {
+      root = new Node(key);
+    }
+    else {
+      insert(root, key);
+    }
+  }
+
+  private int balanceFactor(Node n) {
+    if (n == null) return 0;
+    return height(n.leftChild) - height(n.rightChild);
+  }
+
+  public Node insert(Node n, String key) {
+    //System.out.println(key);
+    // insert
+    if (n == null) {
+      n = new Node(key);
+      // System.out.println(key);
+      // nodes.add(n);
+      return n;
+    }
+
+    if (key.equals(n.key)) {
+      n.frequency += 1;
+      return n;
+    }
+    else if (key.compareTo(n.key) > 0) {
+      n.rightChild = insert(n.rightChild, key);
+    }
+    else if (key.compareTo(n.key) < 0){
+      n.leftChild = insert(n.leftChild, key);
+    }
+    updateHeight(n);
+
+    // rotate
+    if (key.compareTo(n.key) > 0) {
+      if (balanceFactor(n) <= -2) {
+        if (key.compareTo(n.rightChild.key) > 0) {
+          n = rotateLeft(n);
+          // TODO: build fails when execute rotateLeft() ?
+        } else if (key.compareTo(n.rightChild.key) < 0) {
+          n = rotateRightLeft(n);
+        }
+      }
+    }
+    else if (key.compareTo(n.key) < 0) {
+      if (balanceFactor(n) >= 2) {
+        if (key.compareTo(n.leftChild.key) < 0) {
+          n = rotateRight(n);
+        } else if (key.compareTo(n.leftChild.key) > 0) {
+          n = rotateLeftRight(n);
+        }
+      }
+    }
+
+    //updateHeight(n);
+    return n;
+  }
  
 }
 
--- Assignment2/src/BST.java
+++ Assignment2/src/BST.java
@@ -13,26 +13,46 @@ class Node {
     this.rightChild = null;
     this.frequency = 1;
     this.accessCount = 0;
+    this.height = 1;
+  }
+
+  @Override
+  public boolean equals(Object n) {
+    if (!(n instanceof Node)) return false;
+    return this.key.equals(((Node) n).key);
+    /*if (this.key.equals(((Node) n).key)) {
+      System.out.println("true");
+      return true;
+    }
+
+    System.out.println("false");
+    return false;
+    //
+    */
+
   }
 
 }
 
 
 public class BST { // Binary Search Tree implementation
-  ArrayList<Node> nodes;
+  // ArrayList<Node> nodes;
   Node root;
   public BST() {
-    nodes = new ArrayList<>();
+    // nodes = new ArrayList<>();
     root = null;
   }
 
   public void insert(String key) {
     if (root == null) {
       root = new Node(key);
-      nodes.add(root);
+      // nodes.add(root);
     }
     else {
       insert(root, key);
+      // if (!nodes.contains(n)) nodes.add(n);
+
+      // duplicate node?
     }
   }
 
@@ -40,7 +60,7 @@ public class BST { // Binary Search Tree implementation
     // current node n
     if (n == null) {
       n = new Node(key);
-      nodes.add(n);
+      // nodes.add(n);
       return n;
     }
 
@@ -48,28 +68,31 @@ public class BST { // Binary Search Tree implementation
       n.frequency += 1;
       return n;
     }
-    else if (n.key.compareTo(key) > 0) { // n.key > key; ex) B > A
+    else if (key.compareTo(n.key) > 0) { // key > n.key; ex) B > A => key goes to right
       n.rightChild = insert(n.rightChild, key);
     }
     else {
       n.leftChild = insert(n.leftChild, key);
     }
+    // nodes.add(n);
     return n;
   }
 
   public boolean find(String key) {
-    return find(root, key);
+    if (find(root, key) == null) return false;
+    return true;
+    // return find(root, key);
   }
 
-  public boolean find(Node n, String key) {
+  public Node find(Node n, String key) { // public void find
     if (n == null) {
-      return false;
+      return null;
     }
     n.accessCount += 1;
-    if (n.key.equals(key)) {
-      return true;
+    if (key.equals(n.key)) {
+      return n;
     }
-    else if (n.key.compareTo(key) > 0) {
+    else if (key.compareTo(n.key) > 0) {
       return find(n.rightChild, key);
     }
     else {
@@ -78,28 +101,52 @@ public class BST { // Binary Search Tree implementation
   }
 
   public int size() {
-    return this.nodes.size();
+    // return this.nodes.size();
+    return size(root);
+  }
+  public int size(Node n) {
+    if (n == null) return 0;
+    return 1 + size(n.leftChild) + size(n.rightChild);
   }
 
   public int sumFreq() {
-    int sum = 0;
+    /*int sum = 0;
     for (Node n: nodes) {
       sum += n.frequency;
     }
-    return sum;
+    return sum;*/
+    return sumFreq(root);
+  }
+  public int sumFreq(Node n) {
+    if (n == null) return 0;
+    return n.frequency + sumFreq(n.leftChild) + sumFreq(n.rightChild);
   }
   public int sumProbes() {
-    int sum = 0;
+    /*int sum = 0;
     for (Node n: nodes) {
       sum += n.accessCount;
     }
-    return sum;
+    return sum;*/
+    return sumProbes(root);
+  }
+  public int sumProbes(Node n) {
+    if (n == null) return 0;
+    return n.accessCount + sumProbes(n.leftChild) + sumProbes(n.rightChild);
   }
   public void resetCounters() {
-    for (Node n: nodes) {
+    resetCounters(root);
+    /*for (Node n: nodes) {
       n.accessCount = 0;
       n.frequency = 1;
-    }
+    }*/
+  }
+
+  public void resetCounters(Node n) {
+    if (n==null) return;
+    resetCounters(n.leftChild);
+    n.accessCount = 0;
+    n.frequency = 1;
+    resetCounters(n.rightChild);
   }
 
   public void print() {
@@ -108,12 +155,37 @@ public class BST { // Binary Search Tree implementation
 
   public void printInOrder(Node n) {
     // left - root - right
-    if (n == null) return;
-    if (n.leftChild == null) {
+    if (n != null) {
+      printInOrder(n.leftChild);
       System.out.println("["+n.key+":"+n.frequency+":"+n.accessCount+"]");
       printInOrder(n.rightChild);
     }
-    printInOrder(n.leftChild);
+    /* if (n.leftChild == null) {
+      System.out.println("["+n.key+":"+n.frequency+":"+n.accessCount+"]");
+      printInOrder(n.rightChild);
+    }
+    printInOrder(n.leftChild); */
+
+
+  }
+
+  public void printTree() {
+    printTree(root, "", true);
+  }
+  private void printTree(Node currPtr, String indent, boolean last) {
+    if (currPtr != null) {
+      System.out.print(indent);
+      if (last) {
+        System.out.print("R----");
+        indent += "   ";
+      } else {
+        System.out.print("L----");
+        indent += "|  ";
+      }
+      System.out.println(currPtr.key);
+      printTree(currPtr.leftChild, indent, false);
+      printTree(currPtr.rightChild, indent, true);
+    }
   }
 
 }
